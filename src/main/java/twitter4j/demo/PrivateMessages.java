fd29760fdcc45faa1a6f6c90610ffec7e172e0ef
Anoop Elias <anoopelias@gmail.com>
2013-07-01 00:51:27 +0530

    Added skeleton for Private Messages

---
+++ src/main/java/twitter4j/demo/PrivateMessages.java
@@ -0,0 +1,22 @@
+package twitter4j.demo;
+
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.RequestToken;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class PrivateMessages extends HttpServlet {
+
+   public void doGet(HttpServletRequest request, HttpServletResponse response) {
+   }
+
+}
+
